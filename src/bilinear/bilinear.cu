66c4f9b3e23ab7a919fa53d3f4336a451ddff0dc
brod4910 <brod4910@gmail.com>
2024-10-01 22:05:52 -0600

    how to unoptimize a kernel 101

--- src/bilinear/bilinear.cu
+++ src/bilinear/bilinear.cu
@@ -40,6 +40,60 @@ bilinear_interpolation_v1(const T *__restrict__ A, int input_rows,
   B[r * out_cols + c] = pixel;
 }
 
+template <typename T, int TILE_X = 32, int TILE_Y = 32>
+__global__ void
+bilinear_interpolation_v2(const T *__restrict__ A, int input_rows,
+                               int input_cols, T *__restrict__ B, int out_rows,
+                               int out_cols, float scale_y, float scale_x) {
+  int r = blockDim.y * blockIdx.y + threadIdx.y;
+  int c = blockDim.x * blockIdx.x + threadIdx.x;
+  
+  float num_pixels_x = (out_cols - 1.f) / (input_cols - 1.f); // 8 out pixels in x use 1 input pixel
+  float num_pixels_y = (out_rows - 1.f) / (input_rows - 1.f); // 8 out pixels in y use 1 input pixel 
+
+  int x_floor = c;
+  int y_floor = r;
+  int x_ceil = x_floor + 1;
+  int y_ceil = y_floor + 1;
+
+  T ap = __ldg(&A[y_floor * input_cols + x_floor]);
+  T bp = __ldg(&A[y_floor * input_cols + x_ceil]);
+  T cp = __ldg(&A[y_ceil * input_cols + x_floor]);
+  T dp = __ldg(&A[y_ceil * input_cols + x_ceil]);
+
+  float start_y = r * num_pixels_y;
+  float start_x = c * num_pixels_x;
+
+  if (start_y + num_pixels_y > out_cols || start_x + num_pixels_x > out_rows) {
+    return;
+  }
+
+  for (int i = 0; i < (int)num_pixels_y; ++i) {
+    float out_y = start_y + i;
+
+    int out_y_floor = floor(out_y);
+    for (int j = 0; j < (int)num_pixels_x; ++j) {
+      float out_x = start_x + j;
+      int out_x_floor = floor(out_x);
+
+      float d_x = out_x - out_x_floor;
+      float d_y = out_y - out_y_floor;
+
+      float w00 = (1 - d_x) * (1 - d_y);
+      float w01 = d_x * (1 - d_y);
+      float w10 = (1 - d_x) * d_y;
+      float w11 = d_x * d_y;
+      T q00 = ap * w00;
+      T q01 = bp * w01;
+      T q10 = cp * w10;
+      T q11 = dp * w11;
+
+      T pixel = q00 + q01 + q10 + q11;
+      B[out_y_floor * out_cols + out_x_floor] = pixel; 
+    }
+  }
+}
+
 template <typename T, int Rows, int Cols, int Out_Rows, int Out_Cols,
           int TILE_X = 32, int TILE_Y = 32>
 void launch_swizzle_bilinear() {
@@ -70,16 +124,20 @@ void launch_swizzle_bilinear() {
   constexpr float scale_x = (Cols - 1.) / (Out_Cols - 1.);
   constexpr float scale_y = (Rows - 1.) / (Out_Rows - 1.);
   
-  // TODO: only works in the upsampling case
-  constexpr int thread_x = (Out_Cols - 1.) / (Cols - 1.);
-  constexpr int thread_y = (Out_Rows - 1.) / (Rows - 1.);
+  // TODO: only works in the upsampling case since in downsampling,
+  // we sample every Nth input pixel for an output pixel (different access patern).
+  // Using assumption of 128x128 -> 1024x1024
+  constexpr int num_pixels_x = (Out_Cols - 1.) / (Cols - 1.); // 8 out pixels in x use 1 input pixel
+  constexpr int num_pixels_y = (Out_Rows - 1.) / (Rows - 1.); // 8 out pixels in y use 1 input pixel 
+
+  // for every 1 thread, we process 8 elements in the output.
+  // so for block dimenions 32x32 -> we can process 192x192 output pixels
 
   dim3 threadsPerBlock(TILE_X, TILE_Y);
-  dim3 blocksPerGrid(div_up(Out_Rows, threadsPerBlock.y),
-                     div_up(Out_Cols, threadsPerBlock.x));
+  dim3 blocksPerGrid(div_up(Rows, threadsPerBlock.y),
+                     div_up(Cols, threadsPerBlock.x));
 
-
-  bilinear_interpolation_v1<T, TILE_X, TILE_Y>
+  bilinear_interpolation_v2<T, TILE_X, TILE_Y>
       <<<blocksPerGrid, threadsPerBlock>>>(
           A_device, Rows, Cols, B_device, Out_Rows, Out_Cols, scale_y, scale_x);
 
